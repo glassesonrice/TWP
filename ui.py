c2ac76a7d26e535e14efb3b1b990ae4e3e624a92
glassesonrice <67882779+glassesonrice@users.noreply.github.com>
2024-02-11 22:25:09 -0800

    First Commit
    Everything useful is in ui.py for now. Uses PyQt5 to create a window that takes in images. Needs to have venv installed via ">source venv/scripts/activate" or something in git bash, as well as ">pip install PyQt5". For some reason setText() doesn't like to work but it takes widgets perfectly fine.  
    To Do: Make it pretty, find a way to set the text somewhere; Size the image according to the window, not the other way around; output message after file upload

---
+++ ui.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class PhotoLabel(QLabel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setAlignment(Qt.AlignCenter)
+
+
+class Template(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Military Vehicle Identifier'
+        self.setWindowTitle(self.title)
+        
+        sqD = 750
+        self.photo = PhotoLabel()
+        btn = QPushButton('Browse')
+        
+        btn.clicked.connect(self.open_image)
+        grid.addWidget(btn, 1, 0, Qt.AlignHCenter)
+        grid.addWidget(self.photo, 1, 0)
+        self.resize(sqD, sqD)
+
+
+    def open_image(self, filename=None):
+        if not filename:
+            filename, _ = QFileDialog.getOpenFileName(self, 'Select Photo', QDir.currentPath(), 'Images (*.png *.jpg)')
+            if not filename:
+                return
+        self.photo.setPixmap(QPixmap(filename))
+        
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    gui = Template()
+    gui.show()
+    sys.exit(app.exec_())
