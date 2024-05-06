9781ae99d6d67c4f90246b8603d482cc46f0ea34
glassesonrice <67882779+glassesonrice@users.noreply.github.com>
2024-05-05 21:05:08 -0700

    overwrite ui

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from Interpreter import *
 
 class PhotoLabel(QLabel):
 
@@ -10,37 +11,51 @@ class PhotoLabel(QLabel):
         self.setAlignment(Qt.AlignCenter)
 
 
+
 class Template(QWidget):
 
     def __init__(self):
         super().__init__()
         self.title = 'Military Vehicle Identifier'
-        self.setWindowTitle(self.title)
-        
-        sqD = 750
+        self.interpret = Interpreter()
+        self.filename = ''
+        self.string_msg = ''
+        self.setWindowTitle(self.title) # sets title
+        self.win = QMainWindow()
         self.photo = PhotoLabel()
-        btn = QPushButton('Browse')
+        self.sqD = 500 
+        self.strung = QLabel('')
+        self.grid = QGridLayout(self)
+        label = QLabel(self.win)
+        label.setText("Take an image from your file directory and this program will guess what it is")
         
-        btn.clicked.connect(self.open_image)
-        grid = QGridLayout(self)
-        # self.setText('\n\n Drop Image Here \n\n')
-        # grid.addWidget(btn, 1, 0, Qt.AlignHCenter)
-        grid.addWidget(btn, 0, 0, Qt.AlignHCenter)
-        grid.addWidget(self.photo, 1, 0)
-        self.resize(sqD, sqD)
-
+        self.btn0 = QPushButton('Browse') # creates and names button obj
+        
+        self.btn0.clicked.connect(self.open_image) # opens file explorer
+        self.setFixedWidth(750)
+        self.setFixedHeight(750)
+        
+        self.grid.addWidget(label, 0, 0, Qt.AlignHCenter)
+        self.grid.addWidget(self.photo, 1, 0, Qt.AlignHCenter)
+        self.grid.addWidget(self.btn0, 2, 0, Qt.AlignHCenter)
+        self.grid.addWidget(self.strung, 3, 0, Qt.AlignHCenter)
+    
+        self.resize(self.sqD, self.sqD)
 
     def open_image(self, filename=None):
         if not filename:
             filename, _ = QFileDialog.getOpenFileName(self, 'Select Photo', QDir.currentPath(), 'Images (*.png *.jpg)')
             if not filename:
                 return
-        self.photo.setPixmap(QPixmap(filename))
+        self.filename = filename
+        self.update()
+        pmap = QPixmap(filename)
+        self.photo.setPixmap(pmap)
+    
+    def update(self):
+        self.grid.removeWidget(self.strung)
+        self.string_msg = self.interpret.insert_image(self.filename, 'modelA')
+        self.strung = QLabel(self.win)
+        self.strung.setText(self.string_msg)
+        self.grid.addWidget(self.strung, 3, 0, Qt.AlignHCenter)
         
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    gui = Template()
-    gui.show()
-    sys.exit(app.exec_())
